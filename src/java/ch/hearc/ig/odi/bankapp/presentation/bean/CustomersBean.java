f7a1a196af370d23557910c5af1b5b40443d1ea5
Loïc Megert <loic.megert@he-arc.ch>
2015-11-30 18:15:48 +0100

    Création du fichier "customersList.xhtml" et de son Backing Bean "CustomersBean.java".

---
+++ src/java/ch/hearc/ig/odi/bankapp/presentation/bean/CustomersBean.java
@@ -0,0 +1,39 @@
+package ch.hearc.ig.odi.bankapp.presentation.bean;
+
+import ch.hearc.ig.odi.bankapp.business.Customer;
+import ch.hearc.ig.odi.bankapp.services.Services;
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ * Cette classe est un Backing Bean lié à la page "customersList.xhtml".
+ * Elle permet de gérer les clients enregistrés auprès d'une banque.
+ * 
+ * @author Loïc Megert <loic.megert@he-arc.ch>
+ */
+@Named("CustomersBean")
+@RequestScoped
+public class CustomersBean implements Serializable {
+    
+    /** La liste des clients enregistrés auprès d'une banque. */
+    private DataModel<Customer> customers;
+    
+    /** Services mises à disposition du programmeur. */
+    @Inject
+    private Services services;
+    
+    /**
+     * Permet d'obtenir la liste des clients enregistrés auprès d'une banque.
+     * 
+     * @return La liste des clients enregistrés auprès d'une banque.
+     */
+    public DataModel<Customer> getCustomers() {
+        customers = new ListDataModel<>();
+        customers.setWrappedData(services.getCustomersList());
+        return customers;
+    }
+}
