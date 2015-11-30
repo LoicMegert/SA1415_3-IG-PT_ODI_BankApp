d9be1206f2b63b2b19fb673ab5c82f7e76ac03e7
Loïc Megert <loic.megert@he-arc.ch>
2015-11-30 18:42:40 +0100

    "createCustomer.xhtml" complété. Création de son Backing Bean "CustomerCreateBean.java". Fichiers commentés.

---
+++ src/java/ch/hearc/ig/odi/bankapp/presentation/bean/CustomerCreateBean.java
@@ -0,0 +1,95 @@
+package ch.hearc.ig.odi.bankapp.presentation.bean;
+
+import ch.hearc.ig.odi.bankapp.services.Services;
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ * Cette classe est un Backing Bean lié à la page "createCustomer.xhtml".
+ * Elle permet de gérer la création de nouveau client.
+ * 
+ * @author Loïc Megert <loic.megert@he-arc.ch>
+ */
+@Named("CustomerCreateBean")
+@SessionScoped
+public class CustomerCreateBean implements Serializable {
+    
+    /** Le numéro du nouveau client. */
+    private int id;
+    /** Le prénom du nouveau client. */
+    private String firstName;
+    /** Le nom du nouveau client. */
+    private String lastName;
+    
+    /** Services mises à disposition du programmeur. */
+    @Inject
+    private Services services;
+    
+    /**
+     * Effectuer l'enregistrement du nouveau client auprès de la banque.
+     * 
+     * @return "success" pour signaler le succès de l'enregistrement.
+     */
+    public String submit() {
+        services.saveCustomer(id, firstName, lastName);
+        
+        return "success";
+    }
+
+    /**
+     * Permet d'obtenir le numéro du nouveau client.
+     * 
+     * @return Le numéro du nouveau client.
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Modifie le numéro du nouveau client par celui passé en paramètre.
+     * 
+     * @param id Le nouveau numéro du client.
+     */
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    /**
+     * Permet d'obtenir le prénom du nouveau client.
+     * 
+     * @return Le prénom du nouveau client.
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Modifie le prénom du nouveau client par celui passé en paramètre.
+     * 
+     * @param firstName Le nouveau prénom du client.
+     */
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Permet d'obtenir le nom du nouveau client.
+     * 
+     * @return Le nom du nouveau client.
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Modifie le nom du nouveau client par celui passé en paramètre.
+     * 
+     * @param lastName Le nouveau nom du client.
+     */
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+    
+}
